17b0345533ace34942c26ecb62dd54a5d3062005
daffaabp <daffa.budi2003@gmail.com>
2024-12-08 21:43:01 +0700

    add layout, nav, sidebar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="center">
+    <div className="flex h-screen items-center justify-center">
       <h1 className="h1">Storage Management - The only storage solution you need</h1>
     </div>
   );
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,10 +1,11 @@
 "use server"
 
-import { createAdminClient } from "@/lib/appwrite"
+import { createAdminClient, createSessionClient } from "@/lib/appwrite"
 import { appwriteConfig } from "../appwrite/config";
 import { ID, Query } from "node-appwrite";
 import { parseStringify } from "../utils";
 import { cookies } from "next/headers";
+import { avatarPlaceholderUrl } from "@/constants";
 
 const getUserByEmail = async (email: string) => {
     const { databases } = await createAdminClient();
@@ -61,7 +62,7 @@ export const createAccount = async ({
             {
                 fullName,
                 email,
-                avatar: "https://png.pngtree.com/png-vector/20190710/ourmid/pngtree-user-vector-avatar-png-image_1541962.jpg",
+                avatar: avatarPlaceholderUrl,
                 accountId,
             }
         )
@@ -94,3 +95,19 @@ export const verifySecret = async ({
         handleError(error, "Failed to verify OTP");
     }
 };
+
+export const getCurrentUser = async () => {
+    const { databases, account } = await createSessionClient();
+
+    const result = await account.get();
+
+    const user = await databases.listDocuments(
+        appwriteConfig.databaseId,
+        appwriteConfig.usersCollectionId,
+        [Query.equal("accountId", result.$id)],
+    );
+
+    if (user.total <+ 0) return null;
+
+    return parseStringify(user.documents[0]);
+}
